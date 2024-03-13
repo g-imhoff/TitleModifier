09caf88756e080a64a4d08e070905eafb8e09ec8
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-03-13 14:09:51 +0100

    modification of build

---
+++ build/CMakeFiles/TitleModifier.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TitleModifier.dir/src/main.c.o"
+  "CMakeFiles/TitleModifier.dir/src/main.c.o.d"
+  "CMakeFiles/TitleModifier.dir/src/prop_modif.c.o"
+  "CMakeFiles/TitleModifier.dir/src/prop_modif.c.o.d"
+  "TitleModifier"
+  "TitleModifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TitleModifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TitleModifier.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TitleModifier.
+# This may be replaced when dependencies are built.
